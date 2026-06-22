4acd851f29804a6b5b6c4f69842d2932434d1ea6
shenglei <710581786@qq.com>
2026-06-22 12:23:05 +0800

    feat: remove bill-due, template, photography items from quick create sheet

--- test/mobile_redesign_widget_test.dart
+++ test/mobile_redesign_widget_test.dart
@@ -1,4 +1,4 @@
-﻿import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:record_everything/core/theme/app_theme.dart';
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,4 +1,4 @@
-﻿import 'package:record_everything/core/theme/app_theme.dart';
+import 'package:record_everything/core/theme/app_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:record_everything/features/home/widgets/quick_create_sheet.dart';
